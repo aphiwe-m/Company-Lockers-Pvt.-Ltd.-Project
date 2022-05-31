3e7f8ae51d54f2f52087bc2b8affde65e600db52
user <aphiwemambongwe@gmail.com>
2022-05-31 22:01:03 +0200

    First Commit

---
+++ Prototype assessment/src/com/WelcomeClass.java
@@ -0,0 +1,16 @@
+package com;
+
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class WelcomeClass { 
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Eclipse Assessment Project" + "\nThis application is developed by Aphiwe Mbingeleli");
+		
+		
+		    }
+	
+}
